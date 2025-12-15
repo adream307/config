8b00ed9685e3f79e953c49494924dc6728f86ef2
adream307 <adream307@163.com>
2025-12-15 11:33:03 +0800

    proxy
    Signed-off-by: adream307 <adream307@163.com>

--- socks.sh
+++ socks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup autossh -M 0 -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3"  -N -D 1080 huawei-cloud > socks.log 2>&1 &
+nohup autossh -M 0 -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3"  -N -D 1080 proxy-host > socks.log 2>&1 &
